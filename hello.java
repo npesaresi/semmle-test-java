20350d42427e78073fb4c9e3fa92aec24454c6f8
Natalia Pesaresi <natalia.pesaresi@mercadolibre.com>
2020-01-10 09:14:11 -0300

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("5v72h48cl5v6c7fxlor6brvwyof67kbvwcrt7ykwiof6o");
+    }
+
+}
